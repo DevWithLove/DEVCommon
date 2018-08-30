34f4a5dcb915c992777ed4d8675017831f940c37
Tony Mu <tony.mu@fphcare.co.nz>
2018-08-30 15:53:44 +1200

    Added Font

---
+++ DEVCommon/Extensions/UIFontExtensions.swift
@@ -0,0 +1,61 @@
+//
+//  UIFontExtension.swift
+//  DemoApp
+//
+//  Created by Tony Mu on 28/02/2018.
+//  Copyright © 2018 Tony Mu. All rights reserved.
+//
+
+import UIKit
+
+public extension UIFont {
+  
+  public static var TitilliumWeb: TitilliumWebFont {
+    return TitilliumWebFont()
+  }
+  
+  static func loadCustomFont(name: String) ->UIFont {
+    guard let customFont = UIFont(name: name, size: UIFont.labelFontSize) else {
+      fatalError("Failed to load the \(name) font.")
+    }
+    return customFont
+  }
+}
+
+fileprivate protocol CustomFont {
+  var familyName: String {get}
+}
+
+public struct TitilliumWebFont: CustomFont {
+  var familyName: String
+  
+  init() {
+    familyName = "TitilliumWeb"
+  }
+  
+  var regular: UIFont {
+    return UIFont.loadCustomFont(name:  "\(familyName)-Regular")
+  }
+  
+  var light: UIFont {
+    return UIFont.loadCustomFont(name:  "\(familyName)-Light")
+  }
+  
+  var SemiBold: UIFont {
+    return UIFont.loadCustomFont(name:  "\(familyName)-SemiBold")
+  }
+}
+
+public struct Icon: CustomFont {
+  var familyName: String
+  
+  init() {
+    familyName = "FontAwesome"
+  }
+  
+  var regular: UIFont {
+    return UIFont.loadCustomFont(name:  "\(familyName)")
+  }
+}
+
+
